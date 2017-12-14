25ad333e2f01d7c7fe2bef8a6b2663418217a3f1
Evan HE <hyh618@gmail.com>
2017-12-15 00:00:39 +0800

    some init commit

---
+++ ObjectAI/Assets/Scripts/AI/AILoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AILoader : MonoBehaviour {
+	public GameObject Obj;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
